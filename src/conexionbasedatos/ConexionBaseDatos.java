db594a58232b5685efe5a1014f1fc90457cedda3
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-05-24 20:11:02 +0200

    Hacer visible la ventana

--- src/conexionbasedatos/ConexionBaseDatos.java
+++ src/conexionbasedatos/ConexionBaseDatos.java
@@ -17,25 +17,8 @@ public class ConexionBaseDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Metodos objCon=new Metodos();
-        int opcion=0;
-        do{
-        opcion=Integer.parseInt(JOptionPane.showInputDialog("1=Insertar datos\n2=Mostrar datos\n3=Borrar datos\n4=Actualizar datos\n5=Crear la tabla"));
-        switch(opcion){
-            case 1:objCon.insertarDatos();
-            break;
-            case 2:objCon.mostrarDatos();
-            break;
-            case 3:objCon.borrarDatos();
-            break;
-            case 4:objCon.actualizarDatos();
-            break;
-            case 5:objCon.CrearTabla();
-            break;
-//            case 6:objCon.borrarTabla();
-//            break;
-        }
-    }while (opcion!=0);
-    
+        Ventana objV=new Ventana();
+        objV.setVisible(true);
+    }
 }
-}
+
